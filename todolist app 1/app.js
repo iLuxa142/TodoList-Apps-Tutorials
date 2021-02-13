41c9ed7bc770c882798855e3878546adff5c695c
iLuxa142 <iluxa3650@gmail.com>
2021-02-13 23:31:59 +0300

    feat: checkbox func, remove button func

--- todolist app 1/app.js
+++ todolist app 1/app.js
@@ -1,11 +1,37 @@
 Vue.createApp({
     data() {
         return {
-            valueInput: 'New task name'    
+            valueInput: '',
+            needDoList: [],
+            completeList: []
         };
     },
     methods: {
+        handleInput (event) {
+            this.valueInput = event.target.value;
+        },
+        addTask () {
+            if (this.valueInput === '') return;
 
+            this.needDoList.push({
+                title: this.valueInput,
+                id: Math.random()
+            });
+            this.valueInput = ''; 
+        },
+        doCheck (index, type) {
+            if (type === 'need') {
+                const completeTask = this.needDoList.splice(index, 1);
+                this.completeList.push(...completeTask);
+            }
+            else {
+                const noCompleteTask = this.completeList.splice(index, 1);
+                this.needDoList.push(...noCompleteTask);
+            }    
+        },
+        removeTask (index, type) {
+            const toDoList = type === 'need' ? this.needDoList : this.completeList;
+            toDoList.splice(index, 1);
+        }
     }
-
 }).mount('#app');
