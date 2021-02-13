b914b29d345325b3955b6af379d15082fe5d8f3b
iLuxa142 <78208605+iLuxa142@users.noreply.github.com>
2021-02-13 22:06:16 +0300

    add button function (#1)
    * add button function
    
    * test pr 1
    
    * edit @:
    
    * app.js edit

--- todolist app 1/app.js
+++ todolist app 1/app.js
@@ -1,11 +1,23 @@
 Vue.createApp({
     data() {
         return {
-            valueInput: 'New task name'    
+            valueInput: '',
+            needDoList: []
         };
     },
     methods: {
+        handleInput(event) {
+            this.valueInput = event.target.value;
+        },
+        addTask() {
+            if (this.valueInput === '') return;
 
+            this.needDoList.push({
+                title: this.valueInput,
+                id: Math.random()
+            });
+            this.valueInput = ''; 
+        }
     }
 
 }).mount('#app');
