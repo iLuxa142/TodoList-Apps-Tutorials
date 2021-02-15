d5d2d94e852f014a45f87f01aa03c8586b322961
iLuxa142 <iluxa3650@gmail.com>
2021-02-15 20:00:45 +0300

    feat: 1. Change background-color for checked task (CSS). 2. Counters: all and checked tasks

--- todolist app 2 (v2)/app.js
+++ todolist app 2 (v2)/app.js
@@ -2,10 +2,11 @@ new Vue({
     el: "#todo",
     data: {
         newTask: "",
+        checkedTasks: 0,
         taskList: []
     },
     computed: {
-        areAllSelected: function(){
+        areAllChecked: function(){
             return this.taskList.every(function(task){
                 return task.checked;
             }) && this.taskList.length > 0;
@@ -25,16 +26,24 @@ new Vue({
         },
         removeTask: function(task) {
             this.taskList.$remove(task);
+            if (task.checked)
+                this.checkedTasks--;
         },
         clearList: function() {
             this.taskList =[];
+            this.checkedTasks = 0;
         },
-        selectAll: function(task) {
-            var targetValue = this.areAllSelected ? false : true;
+        checkAll: function(task) {
+            const targetValue = this.areAllChecked ? false : true;
 
             for (var i = 0; i < this.taskList.length; i++) {
                 this.taskList[i].checked = targetValue;
             }
+
+            targetValue ? this.checkedTasks = this.taskList.length  : this.checkedTasks = 0;
+        },
+        checkTask: function(task) {
+            task.checked ? this.checkedTasks-- : this.checkedTasks++;
         }
     }
 });
