2c9e0044745c317535cf1dbfda02aca14d532370
iLuxa142 <78208605+iLuxa142@users.noreply.github.com>
2021-02-15 21:31:51 +0300

    feat: 1.Recycle (delete/recover tasks). 2. Add task creation date and time. (#2)
    * feat: 1.Recycle (delete/recover tasks). 2. Add task creation date and time.
    
    * fix: var-const, indent
    
    * fix: split removeTask func
    
    * fix: split removeTask func in js
    
    * fix: split doCheck func
    
    * fix: add placeholder attribute to input
    
    * fix: space between methods;  rename doCheck... functions

--- todolist app 1 (v2)/app.js
+++ todolist app 1 (v2)/app.js
@@ -3,35 +3,61 @@ Vue.createApp({
         return {
             valueInput: '',
             needDoList: [],
-            completeList: []
+            completeList: [],
+            recycleList: []
         };
     },
     methods: {
-        handleInput (event) {
+        handleInput(event) {
             this.valueInput = event.target.value;
         },
-        addTask () {
+
+        addTask() {
             if (this.valueInput === '') return;
+
+            const nowDateTime = new Date();
+            const options = {
+                year: '2-digit',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            };
+            const strDateTime = nowDateTime.toLocaleString("ru", options);
 
             this.needDoList.push({
                 title: this.valueInput,
+                datetime: strDateTime,
                 id: Math.random()
             });
-            this.valueInput = ''; 
+
+            this.valueInput = '';
         },
-        doCheck (index, type) {
-            if (type === 'need') {
-                const completeTask = this.needDoList.splice(index, 1);
-                this.completeList.push(...completeTask);
-            }
-            else {
-                const noCompleteTask = this.completeList.splice(index, 1);
-                this.needDoList.push(...noCompleteTask);
-            }    
+
+        moveToCompleteList(index) {
+            const completeTask = this.needDoList.splice(index, 1);
+            this.completeList.push(...completeTask);
         },
-        removeTask (index, type) {
-            const toDoList = type === 'need' ? this.needDoList : this.completeList;
-            toDoList.splice(index, 1);
+
+        moveToDoList(index) {
+            const noCompleteTask = this.completeList.splice(index, 1);
+            this.needDoList.push(...noCompleteTask);
+        },
+
+        removeTaskFromDoList(index) {
+            const recycleTask = this.needDoList.splice(index, 1);
+            this.recycleList.push(...recycleTask);
+        },
+
+        removeTaskFromCompleteList(index) {
+            const recycleTask = this.completeList.splice(index, 1);
+            this.recycleList.push(...recycleTask);
+        },
+        
+        recoverTaskFromRecycleList(index) {
+            const noCompleteTask = this.recycleList.splice(index, 1);
+            this.needDoList.push(...noCompleteTask);
         }
     }
 }).mount('#app');
