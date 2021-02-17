3f24848a3f24421274e61341be2533e0157a4630
iLuxa142 <iluxa3650@gmail.com>
2021-02-17 21:08:43 +0300

    feat: 1. edit task title/desc. 2. Done list (tasks in one array)

--- todolist-app-3-v2/script.js
+++ todolist-app-3-v2/script.js
@@ -4,15 +4,23 @@ Vue.component('task', {
     methods: {
         task_done() {
             this.$emit('task_done');
+        },
+
+        task_title_edit() {
+            this.$emit('task_title_edit');
+        },
+
+        task_desc_edit() {
+            this.$emit('task_desc_edit');
         }
     },
     template: `
     <div class="task">
+        <input type="checkbox" @change="task_done()" class="task__done" :checked="data.isDone">
         <div>
-            <h3 class="task__title">{{data.title}}</h3>
-            <p class="task__desc">{{data.desc}}</p>
+            <h3 class="task__title" @dblclick="task_title_edit()">{{data.title}}  ({{data.isDone}})</h3>
+            <p class="task__desc" @dblclick="task_desc_edit()">{{data.desc}}</p>
         </div>
-        <button @click="task_done()" class="task__done">✔️</button>
     </div>
     `
 });
@@ -22,27 +30,57 @@ var vue = new Vue({
     data: {
         new_task: {
             title: '',
-            desc: ''
+            desc: '',
+            isDone: false
         },
-        tasks: [
-            {
-                title: 'Доделать проект Х к 31.03.2021',
-                desc: 'Позвонить Х, узнать детали'
-            },
-            {
-                title: 'Доделать проект У к 31.02.2021',
-                desc: 'Запустить сайт'
-            }]
+        Tasks: [{
+            title: 'Доделать проект Х к 31.03.2021',
+            desc: 'Позвонить Х, узнать детали',
+            isDone: false
+        },
+        {
+            title: 'Доделать проект У к 31.02.2021',
+            desc: 'Запустить сайт',
+            isDone: false
+        },
+        {
+            title: 'Доделать проект Z к 31.04.2021',
+            desc: 'Купить Х',
+            isDone: true
+        }]
     },
 
     methods: {
-        delete_task(id) {
-            this.tasks.splice(id, 1);
+        changeStatusTask(id) {
+            if (this.Tasks[id]) {
+                this.Tasks[id].isDone = !this.Tasks[id].isDone;
+            }
         },
 
-        add_task() {
+        editTaskTitle(id) {
+            if (this.Tasks[id]) {
+                const newTaskTitle = prompt("Enter new task title: ", this.Tasks[id].title);
+
+                if (newTaskTitle) {
+                    this.Tasks[id].title = newTaskTitle;
+                }
+            }
+        },
+
+        editTaskDesc(id) {
+            if (this.Tasks[id]) {
+                const newTaskDesc = prompt("Enter new task desc: ", this.Tasks[id].desc);
+
+                if (newTaskDesc) {
+                    this.Tasks[id].desc = newTaskDesc;
+                }
+            }
+        },
+
+        addTask() {
             if (this.new_task.title != '') {
-                this.tasks.push(this.new_task);
+                this.new_task.isDone = false;
+                this.Tasks.push(this.new_task);
                 this.new_task = [];
             }
         }
