e29510c908f0df320c8b1e692d48eee202401967
iLuxa142 <iluxa3650@gmail.com>
2021-02-25 12:49:54 +0300

    refactor: resetTaskInput; feat: task lists counters; desc html format

--- todolist-app-3-v2/script.js
+++ todolist-app-3-v2/script.js
@@ -18,7 +18,7 @@ Vue.component('task', {
         <input type="checkbox" class="task__done" :checked="data.isDone" @change="changeTaskStatus()">
         <div>
             <h3 class="task__title" @dblclick="editTaskTitle()">{{data.title}}</h3>
-            <p class="task__desc" @dblclick="editTaskDesc()">{{data.desc}}</p>
+            <p class="task__desc" @dblclick="editTaskDesc()"><span v-html="data.desc"></span></p>
         </div>
     </div>
     `
@@ -26,29 +26,59 @@ Vue.component('task', {
 
 var vue = new Vue({
     el: '#app',
-    data: {
-        filterText: '',
-        newTask: { title: '', desc: '', isDone: false },
+    data() {
+        return {
+            filterText: '',
+            newTask: { title: '', desc: '', isDone: false },
 
-        taskList: [
-            {
-                title: 'Доделать проект Х к 30.03.2021',
-                desc: 'Позвонить Х, закрыть сделку',
-                isDone: false
-            },
-            {
-                title: 'Доделать проект У к 30.04.2021',
-                desc: 'Запустить сайт Y',
-                isDone: false
-            },
-            {
-                title: 'Доделать проект Z к 30.05.2021',
-                desc: 'Купить Х',
-                isDone: true
-            }
-        ]
+            taskList: [
+                {
+                    title: 'Доделать проект Х к 30.03.2021',
+                    desc: 'Позвонить Х, <br>закрыть <i>сделку</i>',
+                    isDone: false
+                },
+                {
+                    title: 'Доделать проект У к 30.04.2021',
+                    desc: 'Запустить <b>сайт</b> <a href="https://yandex.ru" target="_blank">Yandex</a>',
+                    isDone: false
+                },
+                {
+                    title: 'Доделать проект Z к 30.05.2021',
+                    desc: '<span style="color: red">Купить Х</span>',
+                    isDone: true
+                }
+            ]
+        }
+    },
+    computed: {
+        doTasksCount() {
+            return this.taskList.filter(task => !task.isDone).length;
+        },
+
+        checkedTasksCount() {
+            return this.taskList.filter(task => task.isDone).length;
+        }
     },
     methods: {
+        resetTaskInput() {
+            // Вариант 1 - сбрасывает все данные
+            // Object.assign(this.$data, this.$options.data());
+
+            // Вариант 2 - не работает как нужно
+            // Object.assign(this.newTask, this.$options.data().newTask);
+
+            // Вариант 3 - 
+            this.newTask = { title: '', desc: '', isDone: false };
+
+            // Вариант 4 - 
+            // return { title: '', desc: '', isDone: false }
+
+            // Вариант 5 - у меня с this.$options, заработало при двух вариантах:
+            // this.newTask = this.$options.data().newTask;
+            // или
+            // this.newTask = Object.assign({}, this.$options.data().newTask);
+        },
+
         changeStatusTask(id) {
             if (this.taskList[id]) {
                 this.taskList[id].isDone = !this.taskList[id].isDone;
@@ -76,16 +106,18 @@ var vue = new Vue({
             }
         },
 
-        /*  1. desc записывается из textarea как многострочный, но выводится в шаблон как однострочный.
-            Доработать до многострочного вывода desc?
-            2. Если desc не заполнен при добавлении задачи, то нет возможности его редактировать в будущем, 
-            т.к. выводящий элемент <p> будет иметь нулевой размер. Добавлять пробел в desc, 
-            даже если при создании задачи поле new_task.desc не заполнено?
-        */
         addTask() {
             if (this.newTask.title) {
+                console.log(this.newTask);
+
+                // преобразовать desc для многострочного вывода в <span v-html="data.desc"></span>
+                // Хорошо что поддерживает форматирование, ссылки и тд. 
+                // Но без фильтрации опасно выводить html, созданный пользоватлем (XSS, внедрение js кода). Доработать.
+                // при пустом desc элемент для вывода <p> будет с min-height: 20px, чтобы можно было изменить, кликнув
+                this.newTask.desc = this.newTask.desc.split('\n').join('<br>');
+
                 this.taskList.push(this.newTask);
-                this.newTask = { title: '', desc: '', isDone: false };
+                this.resetTaskInput();
             }
         }
     }
