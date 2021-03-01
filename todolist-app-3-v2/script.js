68976b4dd8351bdc0f57b7f4f86312d41032727a
iLuxa142 <iluxa3650@gmail.com>
2021-03-01 23:24:32 +0300

    refactor: where is my mind!!!

--- todolist-app-3-v2/script.js
+++ todolist-app-3-v2/script.js
@@ -5,7 +5,8 @@ Vue.component('task-list', {
     },
     data() {
         return {
-            newTask: { title: '', desc: '', isDone: false, isEditing: false },
+            itemId: 6,
+            newTask: { id: 0, title: '', desc: '', isDone: false, isEditing: false },
 
             newList: {
                 listName: '',
@@ -21,12 +22,14 @@ Vue.component('task-list', {
                     isAddingTask: false,
                     tasks: [
                         {
+                            id: 1,
                             title: 'Задача 1',
                             desc: 'Описание 1',
                             isDone: false,
                             isEditing: false
                         },
                         {
+                            id: 2,
                             title: 'Задача 2',
                             desc: 'Описание 2',
                             isDone: false,
@@ -40,6 +43,7 @@ Vue.component('task-list', {
                     isAddingTask: false,
                     tasks: [
                         {
+                            id: 3,
                             title: 'Купить материалы',
                             desc: 'контакты поставщика: ...',
                             isDone: true,
@@ -53,6 +57,7 @@ Vue.component('task-list', {
                     isAddingTask: false,
                     tasks: [
                         {
+                            id: 4,
                             title: 'Сыграть партию в Го',
                             desc: 'использовать стратегию Х',
                             isDone: true,
@@ -66,6 +71,7 @@ Vue.component('task-list', {
                     isAddingTask: false,
                     tasks: [
                         {
+                            id: 5,
                             title: 'Задача 6',
                             desc: 'Описание ...',
                             isDone: false,
@@ -76,45 +82,54 @@ Vue.component('task-list', {
             ]
         }
     },
+
     methods: {
         addList() {
-            if (this.newList.listName) {
-                this.taskLists.unshift({ ...this.newList });
-                this.newList = { ...this.$options.data().newList };
-            }
+            if (!this.newList.listName) return;
+
+            this.taskLists.unshift({ ...this.newList });
+            this.newList = { ...this.$options.data().newList };
         },
 
-        addTask(list) {
-            if (this.newTask.title) {
-                list.tasks.unshift({ ...this.newTask });
-                this.clearTaskInput();
-            }
+        addTask(listIndex) {
+            if (!this.newTask.title) return;
+
+            this.newTask.id = this.itemId++;
+            this.taskLists[listIndex].tasks.unshift({ ...this.newTask });
+            this.clearTaskInput();
+
+            // фокус на поле ввода
+            this.focusInputTitle();
         },
 
-        deleteList(index) {
-            if (!this.taskLists[index]) return;
+        deleteList(listIndex) {
+            if (!this.taskLists[listIndex]) return;
 
-            const tasksCount = this.taskLists[index].tasks.length;
-            const answer = confirm('Вы точно хотите удалить список "' + this.taskLists[index].listName +
+            const tasksCount = this.taskLists[listIndex].tasks.length;
+            const answer = confirm('Вы точно хотите удалить список "' + this.taskLists[listIndex].listName +
                 '" содержащий: ' + tasksCount + ' задач?');
 
             if (answer) {
-                this.taskLists.splice(index, 1);
+                this.taskLists.splice(listIndex, 1);
             }
         },
 
-        changeTaskStatus(task) {
-            if (task)
-                task.isDone = !task.isDone;
+        changeTaskStatus(listIndex, taskIndex) {
+            let targetTask = this.taskLists[listIndex].tasks[taskIndex];
+            console.log(this.taskLists[listIndex].tasks[taskIndex].isDone);
+
+            if (!targetTask) return;
         },
 
         toggleTaskEditing(task) {
-            if (task)
-                task.isEditing = !task.isEditing;
+            if (!task) return;
+
+            task.isEditing = !task.isEditing;
         },
 
-        toggleTaskAdding(list) {
-            const isCurrListAdding = list.isAddingTask;
+        toggleTaskAdding(listIndex) {
+            const currList = this.taskLists[listIndex];
+            const isCurrListAdding = currList.isAddingTask;
 
             this.clearTaskInput();
 
@@ -122,18 +137,34 @@ Vue.component('task-list', {
             this.taskLists.forEach(tasklist => tasklist.isAddingTask = false);
 
             // Если в текущем списке форма не была открытой, то открыть 
-            if (isCurrListAdding == list.isAddingTask)
-                list.isAddingTask = !list.isAddingTask;
+            if (isCurrListAdding == currList.isAddingTask) {
+                currList.isAddingTask = !currList.isAddingTask;
+            }
+
+            if (currList.isAddingTask) {
+                this.focusInputTitle();
+            }
         },
 
         clearTaskInput() {
             this.newTask = { ...this.$options.data().newTask };
         },
+
+        focusInputTitle() {
+            // фокус на поле ввода
+            // Вариант c getElementById
+            setTimeout(() => document.getElementById('inputtitle').focus(), 100);
+
+            // Вариант с refs
+            //setTimeout(() => this.$refs['inputtitle'].focus(), 100);
+
+            // Нормальный вариант... найти позже
+        }
     }
 });
 
 
-var vue = new Vue({
+let vue = new Vue({
     el: '#app',
     data() {
         return {
@@ -157,3 +188,29 @@ var vue = new Vue({
         },
     }
 });
+
+/* архив (может потребоваться)
+
+    // фокус на поле ввода
+    // Вариант c getElementById
+    //setTimeout(() => document.getElementById('inputtitle').focus(), 100);
+
+    // Вариант с refs
+    //setTimeout(() => this.$refs['inputtitle'].focus(), 100);
+
+
+moveToBottom = this.taskLists[listIndex].tasks.splice(taskIndex, 1);
+//this.taskLists[listIndex].tasks.push(...moveToBottom);
+
+
+    computed: {
+        /*
+        listSortedByDone(listIndex) {
+            console.log(listIndex);
+            const sortedList = this.taskLists[listIndex].tasks;
+            sortedList.sort((a, b) => a.isDone - b.isDone);
+            return sortedList;
+        }
+    },
+
+*/
