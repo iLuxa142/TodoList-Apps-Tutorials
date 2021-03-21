89fd7b51c520839a343c42000009ccd6c9457df8
iLuxa142 <iluxa3650@gmail.com>
2021-03-21 16:39:42 +0300

    todolist-app-5-v2. Copy source from todolist-app-5

---
+++ todolist-app-5-v2/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="blue darken-3">
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul class="right">
+        <li><router-link to="/" exact active-class="active"><a href="#">Create</a></router-link></li>
+        <li><router-link to="/list" active-class="active"><a href="#">List</a></router-link></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  nav {
+    padding: 0 2rem;
+  }
+</style>
