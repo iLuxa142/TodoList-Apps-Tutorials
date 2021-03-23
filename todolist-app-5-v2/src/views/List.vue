06f4bb788942a5f9d774384d69e893e195060814
iLuxa142 <iluxa3650@gmail.com>
2021-03-24 01:10:51 +0300

    feat: cards view

--- todolist-app-5-v2/src/views/List.vue
+++ todolist-app-5-v2/src/views/List.vue
@@ -1,73 +1,75 @@
 <template>
-  <div class="row">
-    <div class="input-field col s6">
-      <span>Quick add task:</span>
-      <div class="input-field">
-        <input
-          id="title"
-          type="text"
-          v-model="addTaskTitle"
-          @keyup.enter="quickAddTask"
-        />
-        <label for="title">Enter task title and press enter key</label>
-      </div>
-      <h3>List</h3>
+  <div>
+    <div class="row">
+      <div class="input-field col l6">
+        <span>Quick add task:</span>
+        <div class="input-field">
+          <input
+            id="title"
+            type="text"
+            v-model="addTaskTitle"
+            @keyup.enter="quickAddTask"
+          />
+          <label for="title">Enter task title and press enter key</label>
+        </div>
 
-      <span>Filter by status:</span>
-      <button class="btn btn-small" @click="changeFilter(null)">
-        All (no filter)
-      </button>
-      <button class="btn btn-small" @click="changeFilter('active')">
-        Active
-      </button>
-      <button class="btn btn-small" @click="changeFilter('outdated')">
-        Outdated
-      </button>
-      <button class="btn btn-small" @click="changeFilter('completed')">
-        Completed
-      </button>
+        <h3>List</h3>
+        <span>Filter by status:</span>
+        <button class="btn btn-small" @click="changeFilter(null)">
+          All (no filter)
+        </button>
+        <button class="btn btn-small" @click="changeFilter('active')">
+          Active
+        </button>
+        <button class="btn btn-small" @click="changeFilter('outdated')">
+          Outdated
+        </button>
+        <button class="btn btn-small" @click="changeFilter('completed')">
+          Completed
+        </button>
+
+        <p>
+          Toggle view style:
+          <a class="waves-effect waves-light btn" @click="toggleView">
+            <i class="material-icons left">view_list</i>
+            <i class="material-icons left">view_module</i>
+          </a>
+        </p>
+      </div>
     </div>
-    <table v-if="tasks.length" class="tasks">
-      <thead>
-        <tr>
-          <th><i class="tiny material-icons">check</i></th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Tags</th>
-          <th>Date</th>
-          <th>Status</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
+
+    <div class="row">
+      <div v-if="isCardsView">
+        <div
+          class="card col l3"
+          style="margin: 5px"
           v-for="task of displayTasks"
           :key="task.id"
           @dblclick="$router.push('/task/' + task.id)"
         >
-          <td>
-            <label>
-              <input
-                type="checkbox"
-                class="filled-in"
-                :checked="task.status == 'completed'"
-                @click="changeComplete(task.id)"
-              />
-              <span></span>
-            </label>
-          </td>
-          <td>{{ task.title }}</td>
+          <!-- Cards View -->
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">
+              <label>
+                <input
+                  type="checkbox"
+                  class="filled-in"
+                  :checked="task.status == 'completed'"
+                  @click="changeComplete(task.id)"
+                />
+                <span></span>
+              </label>
+              <span>{{ task.title }}</span>
+              <i class="material-icons right">more_vert</i>
+            </span>
+            <p>Tags: {{ task.tags.length }}</p>
+            <p>Date: {{ new Date(task.date).toLocaleDateString() }}</p>
+            <p>Description:</p>
+            <p class="text" style="width: 250px">
+              {{ task.desc }}
+            </p>
 
-          <td>
-            <div class="text">{{ task.desc }}</div>
-          </td>
-          <td>
-            {{ task.tags.length }}
-          </td>
-          <td>{{ new Date(task.date).toLocaleDateString() }}</td>
-          <td>{{ task.status }}</td>
-          <td>
+            <hr />
             <router-link
               tag="button"
               class="btn btn-small"
@@ -75,17 +77,82 @@
             >
               Edit
             </router-link>
-          </td>
-          <td>
             <button class="btn btn-small red" @click="deleteTask(task.id)">
               X
             </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">
+              <i class="material-icons right">close</i>
+            </span>
+            <p>
+              {{ task.desc }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <!-- Table View -->
+        <table v-if="tasks.length" class="tasks">
+          <thead>
+            <tr>
+              <th><i class="tiny material-icons">check</i></th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Tags</th>
+              <th>Date</th>
+              <th>Status</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="task of displayTasks"
+              :key="task.id"
+              @dblclick="$router.push('/task/' + task.id)"
+            >
+              <td>
+                <label>
+                  <input
+                    type="checkbox"
+                    class="filled-in"
+                    :checked="task.status == 'completed'"
+                    @click="changeComplete(task.id)"
+                  />
+                  <span></span>
+                </label>
+              </td>
+              <td>{{ task.title }}</td>
 
-    <p v-else>No tasks</p>
+              <td>
+                <div class="text">{{ task.desc }}</div>
+              </td>
+              <td>
+                {{ task.tags.length }}
+              </td>
+              <td>{{ new Date(task.date).toLocaleDateString() }}</td>
+              <td>{{ task.status }}</td>
+              <td>
+                <router-link
+                  tag="button"
+                  class="btn btn-small"
+                  :to="'/task/' + task.id"
+                >
+                  Edit
+                </router-link>
+              </td>
+              <td>
+                <button class="btn btn-small red" @click="deleteTask(task.id)">
+                  X
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p v-else>No tasks</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -95,6 +162,7 @@ export default {
     return {
       addTaskTitle: "",
       filter: null,
+      isCardsView: false,
     };
   },
   methods: {
@@ -122,6 +190,10 @@ export default {
 
     changeComplete(id) {
       this.$store.dispatch("changeComplete", id);
+    },
+
+    toggleView() {
+      this.isCardsView = !this.isCardsView;
     },
   },
   computed: {
